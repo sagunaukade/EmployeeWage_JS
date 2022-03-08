bafa0dabd95a1ab0e5ad20556c2dae03fa9fc7ed
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-03-08 12:06:26 +0530

    [Saguna]Added Calculate Daily Employee Wage

---
+++ CalculateDAilyEmpWage.js
@@ -0,0 +1,24 @@
+//Calculate Employee Wage UC-2 
+  const IS_PART_TIME = 1;
+  const IS_FULL_TIME = 2;
+  const PART_TIME_HOURS = 4;
+  const FULL_TIME_HOURS = 8;
+  const WAGE_PER_HOUR = 20;
+  {
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    //Switch case
+    switch (empCheck) {
+      case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+      case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+      default:
+        empHrs = 0;
+    }
+    //calculate emp wage formula
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("EmpWage " + empWage);
+  }
